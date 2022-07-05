73efacaf0f8aa34f67ffb610f1cb354c678c37c9
Haris Samingan <muhamad_haris@hotmail.com>
2022-07-05 08:59:13 +0800

    Add redux for auth

---
+++ redux/ducks/blogAuth.js
@@ -0,0 +1,26 @@
+export const LOG_IN = "LOG_IN";
+export const LOG_OUT = "LOG_OUT";
+
+export const logInAction = (payload) => ({
+  // TODO: 1. Add type login action
+  payload,
+});
+
+export const logOutAction = (payload) => ({
+  type: LOG_OUT,
+  payload,
+});
+
+const initialState = {};
+
+export default (state = initialState, action) => {
+  switch (action) {
+    case LOG_IN:
+      return { ...state, token: action.payload };
+    case LOG_OUT:
+      return { ...state, token: null };
+
+    default:
+      return state;
+  }
+};
