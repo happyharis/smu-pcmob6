7ed528b94e2991dae3a73c29f9a1c243cd65b493
Haris Samingan <muhamad_haris@hotmail.com>
2022-07-07 11:45:14 +0800

    Add profile picture

--- screens/AccountScreen.js
+++ screens/AccountScreen.js
@@ -5,14 +5,19 @@ import {
   Text,
   TouchableOpacity,
   View,
+  Image,
 } from "react-native";
 import axios from "axios";
 import { API, API_WHOAMI } from "../constants/API";
 import { useDispatch, useSelector } from "react-redux";
 import { commonStyles, darkStyles, lightStyles } from "../styles/commonStyles";
 import { lightModeAction, darkModeAction } from "../redux/ducks/accountPref";
+import { logOutAction } from "../redux/ducks/blogAuth";
 
 export default function AccountScreen({ navigation }) {
+  const profilePicture = useSelector(
+    (state) => state.accountPrefs.profilePicture
+  );
   const token = useSelector((state) => state.auth.token);
   const [username, setUsername] = useState(null);
   const isDark = useSelector((state) => state.accountPrefs.isDark);
@@ -48,6 +53,7 @@ export default function AccountScreen({ navigation }) {
   }
 
   function signOut() {
+    dispatch(logOutAction());
     navigation.navigate("SignInSignUp");
   }
 
@@ -68,6 +74,7 @@ export default function AccountScreen({ navigation }) {
       <Text style={[styles.title, styles.text, { marginTop: 30 }]}>
         Hello {username} !
       </Text>
+      <Image source={{ uri: profilePicture }} />
       <TouchableOpacity onPress={() => navigation.navigate("Camera")}>
         <Text style={{ marginTop: 10, fontSize: 20, color: "#0000EE" }}>
           No profile picture. Click to take one.
