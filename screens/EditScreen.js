e2966e92991595a803e482884cbf33fbf465bd3e
Haris Samingan <muhamad_haris@hotmail.com>
2022-07-07 11:15:37 +0800

    Add dark mode

--- screens/EditScreen.js
+++ screens/EditScreen.js
@@ -1,14 +1,17 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
-import { commonStyles, lightStyles } from "../styles/commonStyles";
+import { lightStyles, commonStyles, darkStyles } from "../styles/commonStyles";
+import { useSelector } from "react-redux";
 
 export default function EditScreen({ navigation }) {
-
-  const styles = { ...lightStyles, ...commonStyles }
+  const isDark = useSelector((state) => state.accountPrefs.isDark);
+  const styles = { ...commonStyles, ...(isDark ? darkStyles : lightStyles) };
 
   return (
     <View style={styles.container}>
-      <Text style={[styles.text, styles.title, {marginTop: 20}]}>Edit Screen</Text>
+      <Text style={[styles.text, styles.title, { marginTop: 20 }]}>
+        Edit Screen
+      </Text>
     </View>
   );
 }
